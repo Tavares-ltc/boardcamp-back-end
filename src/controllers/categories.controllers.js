7de7a1870f49a092df943667413b529457471bbd
Lucas Tavares <l.tavaresc@hotmail.com>
2022-09-29 23:51:32 -0300

    Added the function to insert a category

--- src/controllers/categories.controllers.js
+++ src/controllers/categories.controllers.js
@@ -2,7 +2,7 @@ import connection from '../database/database.js';
 
 async function listCategories(req, res){
     try {
-    const categories = (await connection.query('SELECT * FROM categories;')).rows[0]
+    const categories = (await connection.query('SELECT * FROM categories;')).rows
         res.status(200)
         res.send(categories)
     } catch (error) {
@@ -12,7 +12,13 @@ async function listCategories(req, res){
 }
 
 async function createCategory(req, res){
-    
+    const name = res.locals.name;
+    try {
+       await connection.query('INSERT INTO categories (name) VALUES($1);',[name]);
+       res.sendStatus(200);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
 
 export {
